7f556b95d42e064ae3843e21acea27b7d14071b1
dyakubovskiy <d.yakubovskiy@dshub.tech>
2025-03-23 21:50:45 +0300

    delete comment code

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,60 +1,3 @@
-// import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-// import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-// import vueEslintParser from 'vue-eslint-parser'
-// import oxlint from 'eslint-plugin-oxlint'
-// import pluginVue from 'eslint-plugin-vue'
-
-// export default defineConfigWithVueTs(
-//   {
-//     name: 'app/files-to-lint',
-//     files: ['**/*.{ts,mts,tsx,vue}']
-//   },
-//   {
-//     name: 'app/files-to-ignore',
-//     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'types/app.d.ts']
-//   },
-//   pluginVue.configs['flat/recommended'],
-//   vueTsConfigs.recommended,
-//   oxlint.configs['flat/recommended'],
-//   skipFormatting,
-//   {
-//     rules: {
-//       'vue/require-default-prop': 'off',
-//       'vue/attribute-hyphenation': 'off',
-//       'vue/html-closing-bracket-newline': [
-//         'error',
-//         {
-//           singleline: 'never',
-//           multiline: 'never'
-//         }
-//       ],
-//       'vue/attributes-order': [
-//         'error',
-//         {
-//           order: [
-//             'DEFINITION',
-//             'LIST_RENDERING',
-//             'CONDITIONALS',
-//             'RENDER_MODIFIERS',
-//             'GLOBAL',
-//             'UNIQUE',
-//             'TWO_WAY_BINDING',
-//             'OTHER_DIRECTIVES',
-//             'OTHER_ATTR',
-//             'EVENTS',
-//             'CONTENT'
-//           ]
-//         }
-//       ]
-//     },
-//     languageOptions: {
-//       parser: vueEslintParser,
-//       ecmaVersion: 'latest',
-//       sourceType: 'module'
-//     }
-//   }
-// )
-
 import eslint from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginVue from 'eslint-plugin-vue'
@@ -79,7 +22,6 @@ export default typescriptEslint.config(
       }
     },
     rules: {
-      // your rules
       'vue/require-default-prop': 'off',
       'vue/attribute-hyphenation': 'off',
       'vue/html-closing-bracket-newline': [
